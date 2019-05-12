65a0f475b00c44d9748bc2eeaa069adb68a35522
Paweł Szudrowicz <pablo.szudrowicz@gmail.com>
2019-05-12 17:01:26 +0200

    Initial Commit

---
+++ AirCondition/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AirCondition
+//
+//  Created by Paweł Szudrowicz on 12/05/2019.
+//  Copyright © 2019 Paweł Szudrowicz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
